7976b6a463f4b27dc30edaff9b2e7799816eb2c5
Carlos Roig <roigcarlo@gmail.com>
2015-04-20 21:30:06 +0200

    Preparing Setup.py for kratos

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 with open('README.rst', 'r') as readme:
     README_TEXT = readme.read()
 
-VERSION = '0.1.3.dev0'
+VERSION = '0.1.0.dev0'
 
 
 def write_version_py(filename=None):
     if filename is None:
         filename = os.path.join(
-            os.path.dirname(__file__), 'simlammps', 'version.py')
+            os.path.dirname(__file__), 'wrappers', 'version.py')
     ver = """\
 version = '%s'
 """
@@ -25,14 +25,13 @@ version = '%s'
 write_version_py()
 
 setup(
-    name='simlammps',
+    name='simkratos',
     version=VERSION,
     author='SimPhoNy, EU FP7 Project (Nr. 604005) www.simphony-project.eu',
-    description='The lammps wrapper for the SimPhoNy framework',
+    description='The Kratos-CFD and Kratos-DEMPack wrappers for the SimPhoNy framework',
     long_description=README_TEXT,
     entry_points={
-        'simphony.engine': ['lammps = simlammps']},
+        'simphony.engine': ['kratos = simkratos']},
     packages=find_packages(),
-    install_requires=["simphony >= 0.1.1",
-                      "pyyaml >= 3.11"]
+    install_requires=["simphony >= 0.1.1"]
     )
