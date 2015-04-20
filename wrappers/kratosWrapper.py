1d854023ebfbb9e653d88fbc0da721e0618e79ac
Carlos Roig <roigcarlo@gmail.com>
2015-04-20 21:48:55 +0200

    Style errros in KratosWrapper.py

--- wrappers/kratosWrapper.py
+++ wrappers/kratosWrapper.py
@@ -1,4 +1,4 @@
-""" Template to convert modelparts from kratos to simphony 
+""" Template to convert modelparts from kratos to simphony
 
 This file show an example of how to utilze the kratos wrappers
 in order to import or export models from KratosMultiphysics
@@ -6,7 +6,8 @@ in order to import or export models from KratosMultiphysics
 """
 
 # Kratos will works with Python3 by default.
-# This line makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+# This line makes KratosMultiphysics backward
+# compatible with python 2.6 and 2.7
 from __future__ import print_function, absolute_import, division
 
 # Simphony Imports
@@ -19,11 +20,12 @@ from simphony.cuds.mesh import Edge as SimphonyEdge
 from simphony.cuds.mesh import Face as SimphonyFace
 from simphony.cuds.mesh import Cell as SimphonyCell
 
-# Kratos Imports 
+# Kratos Imports
 from KratosMultiphysics import *
 
 # Uuid and other dependences
 from uuid import *
+
 
 class KratosWrapper(object):
 
@@ -56,7 +58,7 @@ class KratosWrapper(object):
         pass
 
     # Small kernels to get ( kratos to simp) and set ( simp to kratos)
-    # entity data 
+    # entity data
 
     def getSolutionStepVariable1D(self, data, entity, variable):
         pair = self.variables_dictionary[variable]
@@ -68,8 +70,8 @@ class KratosWrapper(object):
         pair = self.variables_dictionary[variable]
         data.update({
             pair[0]: [
-                entity.GetSolutionStepValue(pair[2]), 
-                entity.GetSolutionStepValue(pair[3]), 
+                entity.GetSolutionStepValue(pair[2]),
+                entity.GetSolutionStepValue(pair[3]),
                 entity.GetSolutionStepValue(pair[4])
             ]
         })
@@ -83,7 +85,7 @@ class KratosWrapper(object):
 
     def setSolutionStepVariable3D(self, data, entity, variable):
         pair = self.variables_dictionary[variable]
-        for i in xrange(0,3):
+        for i in xrange(0, 3):
             entity.SetSolutionStepValue(
                 pair[2+i],
                 data[pair[0]][0+i]
@@ -95,72 +97,81 @@ class KratosWrapper(object):
     def __setNodalData(self, data, node):
         pass
 
-    def __exportKratosNodes(self,src,dst,entitylist=None,data=None):
+    def __exportKratosNodes(self, src, dst, entitylist=None, data=None):
         pass
 
-    def __exportKratosElements(self,src,dst,entitylist=None,data=None):
+    def __exportKratosElements(self, src, dst, entitylist=None, data=None):
         pass
 
-    def __exportKratosConditions(self,src,dst,entitylist=None,data=None):
+    def __exportKratosConditions(self, src, dst, entitylist=None, data=None):
         pass
 
-    def __importKratosNodes(self,src,dst,entitylist=None,data=None):
+    def __importKratosNodes(self, src, dst, entitylist=None, data=None):
         pass
 
-    def __importKratosElements(self,src,dst,entitylist=None,data=None):
+    def __importKratosElements(self, src, dst, entitylist=None, data=None):
         pass
 
-    def __importKratosConditions(self,src,dst,entitylist=None,data=None):
+    def __importKratosConditions(self, src, dst, entitylist=None, data=None):
         pass
 
-    def read_modelpart(self,filename):
+    def read_modelpart(self, filename):
         pass
 
-    def write_modelpart(self,filename):
+    def write_modelpart(self, filename):
         pass
 
-    def add_pc(self,src):
+    def add_pc(self, src):
         self.pcs[src.name] = src
 
-    def add_mesh(self,src):
+    def add_mesh(self, src):
         self.meshes[src.name] = src
 
-    def add_lattice(self,src):
+    def add_lattice(self, src):
         self.lattices[src.name] = src
 
-    def get_pc(self,name):
+    def get_pc(self, name):
         return self.pcs[name]
 
-    def get_mesh(self,name):
+    def get_mesh(self, name):
         return self.meshes[name]
 
-    def get_lattice(self,name):
+    def get_lattice(self, name):
         return self.lattices[name]
 
     def updateForwardDicc(self):
         if not self.uuid_to_id_node_map:
-            self.uuid_to_id_node_map = {v: k for k, v in self.id_to_uuid_node_map.items()}
+            self.uuid_to_id_node_map = {
+                v: k for k, v in self.id_to_uuid_node_map.items()
+            }
         if not self.uuid_to_id_element_map:
-            self.uuid_to_id_element_map = {v: k for k, v in self.id_to_uuid_element_map.items()}
+            self.uuid_to_id_element_map = {
+                v: k for k, v in self.id_to_uuid_element_map.items()
+            }
         if not self.uuid_to_id_condition_map:
-            self.uuid_to_id_condition_map = {v: k for k, v in self.id_to_uuid_condition_map.items()}
+            self.uuid_to_id_condition_map = {
+                v: k for k, v in self.id_to_uuid_condition_map.items()
+            }
 
     def updateBackwardDicc(self):
         if not self.id_to_uuid_node_map:
-            self.id_to_uuid_node_map = {v: k for k, v in self.uuid_to_id_node_map.items()}
+            self.id_to_uuid_node_map = {
+                v: k for k, v in self.uuid_to_id_node_map.items()
+            }
         if not self.id_to_uuid_element_map:
-            self.id_to_uuid_element_map = {v: k for k, v in self.uuid_to_id_element_map.items()}
+            self.id_to_uuid_element_map = {
+                v: k for k, v in self.uuid_to_id_element_map.items()
+            }
         if not self.id_to_uuid_condition_map:
-            self.id_to_uuid_condition_map = {v: k for k, v in self.uuid_to_id_condition_map.items()}
+            self.id_to_uuid_condition_map = {
+                v: k for k, v in self.uuid_to_id_condition_map.items()
+            }
 
-    def setMeshData(self,mesh):
+    def setMeshData(self, mesh):
         pass
 
     def initialize(self):
         pass
 
-    def run(self,mesh):
+    def run(self, mesh):
         pass
-
-
-
