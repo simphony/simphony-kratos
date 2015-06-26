3b02573d72108541184d1d0ca97ddd354e7b3f9e
Carlos Roig <roigcarlo@gmail.com>
2015-06-26 15:55:46 +0200

    old "wrappers" import

--- simkratos/tests/test_kratos_wrapper.py
+++ simkratos/tests/test_kratos_wrapper.py
@@ -10,7 +10,7 @@ import unittest
 from simphony.cuds.mesh import Mesh, Point, Edge, Face, Cell
 from simphony.core.data_container import DataContainer
 
-from wrappers.kratosWrapper import KratosWrapper
+from simkratos.kratosWrapper import KratosWrapper
 
 
 class TestKratosWrapper(unittest.TestCase):
