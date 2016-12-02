4f817e50efc3597cfad66e8c313cc58825fcb5ac
roigcarlo <roigcarlo@gmail.com>
2016-12-02 09:41:52 +0100

    Unittest accidentaly removed

--- simkratos/tests/cfd/test_kratos_cfd.py
+++ simkratos/tests/cfd/test_kratos_cfd.py
@@ -1,9 +1,11 @@
-""" This files provide a test example on how to use
-Simphony along with the Kratos Multiphisics CFD wrapper
+""" test_kratosWrapper module
+
+This module contains the unitary tests for the
+kratosWrapper class.
 
 """
-
 import os
+import unittest
 
 from simphony.core.cuba import CUBA
 
@@ -14,37 +16,65 @@ from simphony.cuds.meta import api
 from simphony.engine import EngineInterface
 from simphony.engine import kratos
 
-# Add the problem path to the script
-path = os.path.join(os.path.dirname(__file__), "CFD_exampleFluid")
 
-cuds = CUDS(name='example_kratos_cfd_simulatiob')
+class TestKratosCFDWrapper(unittest.TestCase):
 
-# Integration time:
-itime = api.IntegrationTime(name="cfd_integration_time")
-itime.time = 0.0001
-itime.step = 0.0025
-itime.final = 0.0125  # 5 Kratos Timesteps
-cuds.add(itime)
+    def setUp(self):
+        """ Creates the necessary information tu run the tests
 
-# Utils are used to read an existing Kratos model as raw data so we can
-# initialize the correct simphony datasets
-utils = kratos.CFD_Utils()
+        """
 
-# Reads Kratos mpda as a simphony data.
-kratos_model = utils.read_modelpart(path)
+        self.path = os.path.join(
+            os.path.dirname(__file__),
+            "CFD_exampleFluid"
+        )
 
-# Add the datasets readed from the conversor.
-for dataset in kratos_model['datasets']:
-    cuds.add(dataset)
+        self.time_step = 0.001
+        self.num_steps = 5
 
-# Add the boundary contitions from the conversor
-for condition in kratos_model['conditions']:
-    cuds.add(condition)
+    def test_run(self):
+        """ Test if cfd can run
 
-# Add the materials contitions from the conversor
-for material in kratos_model['materials']:
-    cuds.add(material)
+        """
 
-# Create the simulation and run the problem
-sim = Simulation(cuds, "KRATOS_CFD", engine_interface=True)
-sim.run()
+        # Add the problem path to the script
+        path = os.path.join(os.path.dirname(__file__), "CFD_exampleFluid")
+
+        cuds = CUDS(name='example_kratos_cfd_simulatiob')
+
+        # Integration time:
+        itime = api.IntegrationTime(name="cfd_integration_time")
+        itime.time = 0.0001
+        itime.step = 0.0025
+        itime.final = 0.0125  # 5 Kratos Timesteps
+        cuds.add(itime)
+
+        # Utils are used to read an existing Kratos model as raw data so we can
+        # initialize the correct simphony datasets
+        utils = kratos.CFD_Utils()
+
+        # Reads Kratos mpda as a simphony data.
+        model = utils.read_modelpart(path)
+
+        # Add the datasets readed from the conversor.
+        for dataset in model['datasets']:
+            cuds.add(dataset)
+
+        # Add the boundary contitions from the conversor
+        for condition in model['conditions']:
+            cuds.add(condition)
+
+        # Add the materials contitions from the conversor
+        for material in model['materials']:
+            cuds.add(material)
+
+        # Create the simulation and run the problem
+        sim = Simulation(cuds, "KRATOS_CFD", engine_interface=True)
+        sim.run()
+
+    def tear_down(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- simkratos/tests/dem/test_kratos_dem.py
+++ simkratos/tests/dem/test_kratos_dem.py
@@ -1,9 +1,11 @@
-""" This files provide a test example on how to use
-Simphony along with the Kratos Multiphisics DEM wrapper
+""" test_kratosWrapper module
+
+This module contains the unitary tests for the
+kratosWrapper class.
 
 """
-
 import os
+import unittest
 
 from simphony.core.cuba import CUBA
 
@@ -14,41 +16,73 @@ from simphony.cuds.meta import api
 from simphony.engine import EngineInterface
 from simphony.engine import kratos
 
-# Add the problem path to the script
-pathParticles = os.path.join(os.path.dirname(__file__), "3balls")
-pathSolid = os.path.join(os.path.dirname(__file__), "3ballsDEM_FEM_boundary")
 
-cuds = CUDS(name='example_kratos_dem_somulation')
+def abs_path(relPath):
+    return os.path.join(os.path.dirname(__file__), relPath)
 
-# Integration time:
-itime = api.IntegrationTime(name="dem_integration_time")
-itime.time = 0.0001
-itime.step = 0.001
-itime.final = 600 * itime.step
-cuds.add(itime)
 
-# Utils are used to read an existing Kratos model as raw data so we can
-# initialize the correct simphony datasets
-utils = kratos.DEM_Utils()
+class TestKratosCFDWrapper(unittest.TestCase):
 
-# Reads Kratos mpda as a simphony data.
-kratos_model_particles = utils.read_modelpart_as_particles(pathParticles)
-kratos_model_solid = utils.read_modelpart_as_mesh(pathSolid)
+    def setUp(self):
+        """ Creates the necessary information tu run the tests
 
-# Add all models
-for kratos_model in [kratos_model_particles, kratos_model_solid]:
-    # Add the datasets readed from the conversor.
-    for dataset in kratos_model['datasets']:
-        cuds.add(dataset)
+        """
 
-    # Add the boundary contitions from the conversor
-    for condition in kratos_model['conditions']:
-        cuds.add(condition)
+        self.path = os.path.join(
+            os.path.dirname(__file__),
+            "CFD_exampleFluid"
+        )
 
-    # Add the materials contitions from the conversor
-    for material in kratos_model['materials']:
-        cuds.add(material)
+        self.time_step = 0.001
+        self.num_steps = 5
 
-# Create the simulation and run the problem
-sim = Simulation(cuds, "KRATOS_DEM", engine_interface=True)
-sim.run()
+    def test_run(self):
+        """ Test if cfd can run
+
+        """
+
+        # Add the problem path to the script
+        pathParticles = abs_path("3balls")
+        pathSolid = abs_path("3ballsDEM_FEM_boundary")
+
+        cuds = CUDS(name='example_kratos_dem_somulation')
+
+        # Integration time:
+        itime = api.IntegrationTime(name="dem_integration_time")
+        itime.time = 0.0001
+        itime.step = 0.001
+        itime.final = 600 * itime.step
+        cuds.add(itime)
+
+        # Utils are used to read an existing Kratos model as raw data so we can
+        # initialize the correct simphony datasets
+        utils = kratos.DEM_Utils()
+
+        # Reads Kratos mpda as a simphony data.
+        model_particles = utils.read_modelpart_as_particles(pathParticles)
+        model_solid = utils.read_modelpart_as_mesh(pathSolid)
+
+        # Add all models
+        for model in [model_particles, model_solid]:
+            # Add the datasets readed from the conversor.
+            for dataset in model['datasets']:
+                cuds.add(dataset)
+
+            # Add the boundary contitions from the conversor
+            for condition in model['conditions']:
+                cuds.add(condition)
+
+            # Add the materials contitions from the conversor
+            for material in model['materials']:
+                cuds.add(material)
+
+        # Create the simulation and run the problem
+        sim = Simulation(cuds, "KRATOS_DEM", engine_interface=True)
+        sim.run()
+
+    def tear_down(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
