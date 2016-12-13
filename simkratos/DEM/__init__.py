e0f38e64ac73551a746ec4657a033fb74e6dea97
roigcarlo <roigcarlo@gmail.com>
2016-12-13 16:26:18 +0100

    removed indirection

--- simkratos/DEM/__init__.py
+++ simkratos/DEM/__init__.py
@@ -1,6 +1,6 @@
 from ..kratos_utils import DEM_Utils
 from ..cuba_extension import CUBAExt
-from ..DEM.kratos_DEM_wrapper import DEMWrapper
+from .kratos_DEM_wrapper import DEMWrapper
 
 __all__ = [
     "DEM_Utils",
