f95c2e66841c49d930f33af3bc56f763e93be041
Stefano Borini <sborini@enthought.com>
2017-01-12 11:11:52 +0000

    Wonder if compatible allows for dev versions

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
     packages=find_packages(),
     package_data={'simkratos': ['tests/dem/*.mdpa',
                                 'tests/cfd/*.mdpa']},
-    install_requires=["simphony >= 0.5.0"]
+    install_requires=["simphony~=0.5"]
 )
