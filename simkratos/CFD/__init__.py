4c107f702e5eee47e6ec8abf448a10abae7212db
Stefano Borini <sborini@enthought.com>
2017-01-12 11:25:42 +0000

    Fixed missing import

--- simkratos/CFD/__init__.py
+++ simkratos/CFD/__init__.py
@@ -1,6 +1,7 @@
 from ..kratos_utils import CFD_Utils
 from ..cuba_extension import CUBAExt
 from .kratos_CFD_wrapper import CFDWrapper
+from . import ProjectParameters
 
 __all__ = [
     "CFD_Utils",
