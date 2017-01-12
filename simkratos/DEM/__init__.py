91b9db1a0996ff2de7912f5d1bf7e7e8ce0b9324
Stefano Borini <sborini@enthought.com>
2017-01-12 12:47:44 +0000

    Added import of DEM_explicit_solver_var

--- simkratos/DEM/__init__.py
+++ simkratos/DEM/__init__.py
@@ -1,9 +1,11 @@
 from ..kratos_utils import DEM_Utils
 from ..cuba_extension import CUBAExt
 from .kratos_DEM_wrapper import DEMWrapper
+from . import DEM_explicit_solver_var
 
 __all__ = [
     "DEM_Utils",
     "CUBAExt",
-    "DEMWrapper"
+    "DEMWrapper",
+    "DEM_explicit_solver_var"
 ]
