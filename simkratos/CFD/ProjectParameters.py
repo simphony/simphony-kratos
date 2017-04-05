c9cca7578786fe881184a1848b950f1d9d68366c
Carlos Roig <roigcarlo@gmail.com>
2017-04-05 10:37:29 +0200

    Updating default parameters

--- simkratos/CFD/ProjectParameters.py
+++ simkratos/CFD/ProjectParameters.py
@@ -3,65 +3,50 @@ domain_size = 3
 
 # Fluid solver configuration
 class FluidSolverConfiguration:
-    solver_type =  "vms_fractional_step_solver"
+    solver_type = "vms_monolithic_solver"
     domain_size = 3
     TurbulenceModel = "None"
 
-    # Velocity solver
-    class velocity_linear_solver_config:
-        solver_type = "BiConjugate gradient stabilized"
-        tolerance = 1E-5
-        max_iteration = 100
-        preconditioner = "ILU0"
-        scaling = False
-    # Pressure solver
-    class pressure_linear_solver_config:
+    # Monolithic solver
+    class linear_solver_config:
         solver_type = "AMGCL"
-        tolerance = 1E-5
-        max_iteration = 100
-        preconditioner = "ILU0"
+        tolerance = 1E-6
+        max_iteration = 5000
+        preconditioner = "Diagonal"
         scaling = False
-        krylov_type = "CG"
-        smoother_type = "DAMPED_JACOBI"
 
     #convergence criteria settings
-    vel_toll = 1E-3
-    press_toll = 1E-3
-    divergence_cleareance_step = 50
+    velocity_relative_tolerance = 1E-3
+    velocity_absolute_tolerance = 1E-6
+    pressure_relative_tolerance = 1E-3
+    pressure_absolute_tolerance = 1E-6
+    divergence_cleareance_step = 0
 
     #other solver settings
-    oss_switch = 1
+    oss_switch = 0
     compute_reactions = True
     time_order = 2
     predictor_corrector = False
     dynamic_tau = 0.01
-    max_vel_its = 4
-    max_press_its = 3
-    laplacian_form = 1
+    max_iteration = 10
+    laplacian_form = 2
 
 #general problem settings
 AutomaticDeltaTime = "Fixed"
-Dt = 0.001
+Dt = 0.01
 Start_time = 0.0
 max_time = 1
-nsteps = 1000
+nsteps = 100
 
+Use_slip_conditions = True
 
 groups_dictionary = {
-        "All" : 1,
-                   }
+        "Volume" : 1,
+}
+
 #output settings
 output_time = 0.1
-output_step = 100
+output_step = 10
 VolumeOutput = True
 nodal_results=["VELOCITY","PRESSURE","REACTION"]
 gauss_points_results=[]
-GiDPostMode = "Binary"
-GiDWriteMeshFlag = True
-GiDWriteConditionsFlag = True
-GiDWriteParticlesFlag = False
-GiDMultiFileFlag = "Single"
-
-problem_name="CFD_exampleFluid"
-problem_path="/home/roigcarlos/Git/simphony-kratos/CFD_example.gid"
-kratos_path="D:\Kratos"
